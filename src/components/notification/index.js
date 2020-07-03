5eaa249963f26bf255434c0e9c4f6a9169c28946
Vladimir <list109@list.ru>
2020-07-03 13:45:11 +0300

    the js implementation of notification

---
+++ src/components/notification/index.js
@@ -0,0 +1,48 @@
+export default class NotificationMessage {
+    constructor(message = 'Hello World', { duration = 5000, type = 'success' } = {}) {
+        NotificationMessage.prevElem;
+
+        this.message = message;
+        this.duration = duration;
+        this.type = type;
+        this.element;
+        
+        this.render();
+    }
+
+    render() {
+        const element = document.createElement('div');
+        element.innerHTML = this.template;
+        this.element = element.firstElementChild;
+    }
+
+    get template() {
+        return `
+            <div class="notification ${this.type}" style="--value:${this.duration / 1000}s">
+                <div class="timer"></div>
+                <div class="notification__content">
+                    <div class="notification-header">${this.type}</div>
+                    <div class="notification-body">
+                        ${this.message}
+                    </div>
+                </div>
+            </div>
+        `;
+    }
+
+    show(parent = document.body) {
+        NotificationMessage.prevElem?.remove();
+
+        NotificationMessage.prevElem = this.element;
+        parent.append(this.element);
+        setTimeout(() => { this.destroy() }, this.duration);
+    }
+
+    remove() {
+        this.element.remove();
+    }
+
+    destroy() {
+        this.remove();
+    }
+}
